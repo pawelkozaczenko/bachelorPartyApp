85c6a492e30481eb299747e8330ecb11203cb7ac
Pawel <pawelkozaczenko@gmail.com>
2024-07-05 15:22:08 +0200

    fix adjustment to render.com settings

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 4000;
 app.get('/', function (req, res) {
   res.send('Welcome to Bachelor Party App!');
 });
-app.listen(8080, function () {
-  console.log('Bachelor Party listening on port ${port}');
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 });
