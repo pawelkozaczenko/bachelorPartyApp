e2659a71008a66ffb119ab3b3c1b1b93b2454de4
Pawel <pawelkozaczenko@gmail.com>
2024-07-06 07:47:53 +0200

    add prod db settings

--- dbConfig.js
+++ dbConfig.js
@@ -6,8 +6,10 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
+const connectionStringProd = `postgresql://${process.env.DB_USER_P}:${process.env.DB_PASSWORD_P}@${process.env.DB_HOST_P}:${process.env.DB_PORT_P}/${process.env.DB_DATABASE_P}`;
+
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  connectionString: isProduction ? connectionStringProd : connectionString,
   //ssl: isProduction
 });
 
